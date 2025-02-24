e8e450c7e72852cef3bf322eddd7926a66619cb5
96karishmaupadhyay <karishmaupadhyay1996@gmail.com>
2025-02-24 20:40:54 +0530

    Updated Navbar and added menu icon

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
 import "./navbar.css" 
 import logo from "../../assets/logo.png"
+import menu from "../../assets/menu.png"
 const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg bg-light" >
   <div className="container-fluid">
     <a className="navbar-brand" href="#" >
-      <img src={logo} alt=""  height={60} width={120}/>
+      <img src={logo} alt=""  height={40} width={120}/>
     </a>
     <button className="navbar-toggler light" type="button" data-bs-toggle="collapse"  data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"  ></span>
+      {/* <span className="navbar-toggler-icon"  ></span> */}
+      <img src={menu} alt="" />
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -26,9 +28,7 @@ const Navbar = () => {
         <li className="nav-item">
           <a className="nav-link" href="#values" >Our Values</a>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#visit">Visit us at</a>
-        </li>
+       
       </ul>
       <div>
         <a href="#contact">
